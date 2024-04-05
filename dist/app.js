959718216f8d9fbc3b1421221a62c04f2f05d721
Konstantin <k.kalnizki13@gmail.com>
2024-04-05 20:35:42 +0600

    feat: Card Stylization

--- dist/app.js
+++ dist/app.js
@@ -1101,7 +1101,7 @@
     class Card extends DivComponent {
         constructor(appState, cardState) { 
             super();
-            this.appStatestate = appState; 
+            this.appState = appState; 
             this.cardState = cardState; // Состояние карточки
         }
 
@@ -1109,32 +1109,32 @@
     // Верстка карточки
         render() {
             this.el.classList.add('card');
-            const existInFavorites = this.appStatestate.favorites.find(
+            const existInFavorites = this.appState.favorites.find(
                 b => b.key == this.cardState.key
             );
             this.el.innerHTML = `
-            <div class="card__image">
-                <img src="https://covers.openlibrary.org/b/olid/${this.cardState.cover_edition_key}-M.jpg" alt="Обложка" />
+        <div class="card__image">
+            <img src="https://covers.openlibrary.org/b/olid/${this.cardState.cover_edition_key}-M.jpg" alt="Обложка" />
+        </div>
+        <div class="card__info">
+            <div class="card__tag">
+                ${this.cardState.subject ? this.cardState.subject[0] : 'Не найдено'}
             </div>
-            <div class="card__info>
-                <div class="card__tag">
-                    ${this.cardState.subject ? this.cardState.subject[0] : 'Не задано'}
-                </div>
-                <div class="card__name">
-                    ${this.cardState.title}
-                </div>
-                <div class="card__author">
-                    ${this.cardState.author_name ? this.cardState.author_name[0] : 'Не задано'}
-                </div>
-                <div class="card__footer">
-                    <button class="button_add ${existInFavorites ? 'button__active' : ''}">
-                        ${existInFavorites
-                            ? '<img src="/static/FavoriteLogo.svg"/>'
-                            : '<img src="/static/Favorite-White.svg"/>'
-                        }
-                    </button>
-                </div>
+            <div class="card__name">
+                ${this.cardState.title}
             </div>
+             <div class="card__author">
+                ${this.cardState.author_name ? this.cardState.author_name[0] : 'Не найдено'}
+            </div>
+            <div class="card__footer">
+                <button class="button__add ${existInFavorites ? 'button__active' : ''}">
+                    ${existInFavorites
+                        ? '<img src="/static/FavoriteLogo.svg" />'
+                        : '<img src="/static/Favorite-white.svg" />'
+                    }
+                </button>
+            </div>
+        </div>
         `;
             return this.el;
         }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -6,7 +6,7 @@ import './card.css'
 export class Card extends DivComponent {
     constructor(appState, cardState) { 
         super();
-        this.appStatestate = appState; 
+        this.appState = appState; 
         this.cardState = cardState; // Состояние карточки
     }
 
@@ -14,32 +14,32 @@ export class Card extends DivComponent {
 // Верстка карточки
     render() {
         this.el.classList.add('card');
-        const existInFavorites = this.appStatestate.favorites.find(
+        const existInFavorites = this.appState.favorites.find(
             b => b.key == this.cardState.key
         );
         this.el.innerHTML = `
-            <div class="card__image">
-                <img src="https://covers.openlibrary.org/b/olid/${this.cardState.cover_edition_key}-M.jpg" alt="Обложка" />
+        <div class="card__image">
+            <img src="https://covers.openlibrary.org/b/olid/${this.cardState.cover_edition_key}-M.jpg" alt="Обложка" />
+        </div>
+        <div class="card__info">
+            <div class="card__tag">
+                ${this.cardState.subject ? this.cardState.subject[0] : 'Не найдено'}
             </div>
-            <div class="card__info>
-                <div class="card__tag">
-                    ${this.cardState.subject ? this.cardState.subject[0] : 'Не задано'}
-                </div>
-                <div class="card__name">
-                    ${this.cardState.title}
-                </div>
-                <div class="card__author">
-                    ${this.cardState.author_name ? this.cardState.author_name[0] : 'Не задано'}
-                </div>
-                <div class="card__footer">
-                    <button class="button_add ${existInFavorites ? 'button__active' : ''}">
-                        ${existInFavorites
-                            ? '<img src="/static/FavoriteLogo.svg"/>'
-                            : '<img src="/static/Favorite-White.svg"/>'
-                        }
-                    </button>
-                </div>
+            <div class="card__name">
+                ${this.cardState.title}
             </div>
+             <div class="card__author">
+                ${this.cardState.author_name ? this.cardState.author_name[0] : 'Не найдено'}
+            </div>
+            <div class="card__footer">
+                <button class="button__add ${existInFavorites ? 'button__active' : ''}">
+                    ${existInFavorites
+                        ? '<img src="/static/FavoriteLogo.svg" />'
+                        : '<img src="/static/Favorite-white.svg" />'
+                    }
+                </button>
+            </div>
+        </div>
         `
         return this.el;
     }
